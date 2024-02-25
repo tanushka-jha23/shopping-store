fe315f3e29ef7f0404e567ae6bf66e1c27630a5a
tanushka-jha23 <tanushkajha670@gmail.com>
2024-02-25 17:29:37 +0530

    added structure and style

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let state = {
-    itemlist : [],
+    itemlist : [Mac, iPhone, iPad, appleWatch, Airpods, AirTag, HomePod, TV4K, Accessories],
     wishlist : [],
 
-    
+    additem(i) {
+        this.wishlist.push(this.itemList[i])
+    },
+
+    subscribers: [],
+    subscribe(f){
+        this.subscribers.push(f)
+    },
+
+    publish(){
+        let i = 0
+        for(i = 0; i < this.subscribers.length; i++){
+            this.subscribers[i]()
+        }
+    }
 }
