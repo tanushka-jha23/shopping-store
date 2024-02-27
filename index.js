a6283a7848e3c48081b38337602daac3f9af244a
tanushka-jha23 <tanushkajha670@gmail.com>
2024-02-27 19:18:21 +0530

    added reactive js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ let state = {
     itemlist : [Mac, iPhone, iPad, appleWatch, Airpods, AirTag, HomePod, TV4K, Accessories],
     wishlist : [],
 
-    additem(i) {
-        this.wishlist.push(this.itemList[i])
-    },
-
     subscribers: [],
     subscribe(f){
         this.subscribers.push(f)
@@ -16,5 +12,32 @@ let state = {
         for(i = 0; i < this.subscribers.length; i++){
             this.subscribers[i]()
         }
+    },
+
+    additem(i) {
+        this.wishlist.push(this.itemList[i])
+        this.publish()
     }
+}
+
+const wishList = document.querySelector(".wishlist");
+const like = document.querySelectorAll(".like");
+const itemList = document.querySelectorAll("#item");
+
+const addItemList = () => {
+    wishList = ""
+    let i = 0
+    while(i < state.wishlist.length){
+        let items = document.createElement("p")
+        items.textcontent = state.wishlist[i]
+        wishList.appendChild(items)
+
+        i = i + 1
+    }
+}
+subscribe(addItemList)
+
+let i = 0 
+for(i = 0; i < like.length; i++){
+    like[i].addEventListener("click", additem(parseInt(getElementsByClassName("like").id)))
 }
